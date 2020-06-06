006398139f4a8ec2ff6bad9d85919455ea2ddb53
Santosh Marigowda <santosharakere@gmail.com>
2020-06-06 15:21:32 +0100

    adding product by name to the cart

--- cypress/integration/examples/test1.js
+++ cypress/integration/examples/test1.js
@@ -7,10 +7,23 @@ describe("my first cypress test", function () {
     cy.wait(2000);
     cy.get(".products .product").should("have.length", 4);
     cy.get(".product:visible").should("have.length", 4);
-    cy.get('.products').find('.product').should('have.length', 4);
+    cy.get(".products").find(".product").should("have.length", 4);
 
     // cy.get('.products').find('.product').eq(1).find('.product-action button').click();
-    cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click();
+    // cy.get('.products').find('.product').eq(1).contains('ADD TO CART').click();
 
+    // Add Carrot to the Cart
+    cy.get(".products")
+      .find(".product")
+      .each((element) => {
+        cy.get(element)
+          .find(".product-name")
+          .invoke("text")
+          .then((text) => {
+            if (text.toLowerCase().indexOf("carrot") >= 0) {
+              cy.get(element).contains("ADD TO CART").click();
+            }
+          });
+      });
   });
 });
