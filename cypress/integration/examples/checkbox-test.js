36d23fab20b3f4a33273912349133e383b8cbd13
Santosh Marigowda <santosharakere@gmail.com>
2020-06-06 19:07:06 +0100

    checking multiple checkboxes

--- cypress/integration/examples/checkbox-test.js
+++ cypress/integration/examples/checkbox-test.js
@@ -3,5 +3,8 @@ describe('Checkbox example', () => {
         cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
         cy.get('#checkBoxOption1').check().should('be.checked').and('have.value', 'option1');
         cy.get('#checkBoxOption1').uncheck().should('not.be.checked');
+
+        cy.get('#checkbox-example').get('input[type="checkbox"]').check(['option1', 'option2', 'option3']);
+
     })
 })
