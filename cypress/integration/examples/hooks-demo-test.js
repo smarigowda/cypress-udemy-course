b1b575a914e12f28b0d816b2788f6792ae4c0094
Santosh Marigowda <santosharakere@gmail.com>
2020-06-07 18:45:01 +0100

    enter name and gender

--- cypress/integration/examples/hooks-demo-test.js
+++ cypress/integration/examples/hooks-demo-test.js
@@ -1,12 +1,16 @@
 describe("Hooks Demo", () => {
   before('before block', () => {
     cy.log('before block')
+    cy.visit('https://rahulshettyacademy.com/angularpractice/');
   });
   after('after block', () => {
     cy.log('after  block');
+    cy.get('input[name="name"]:nth-child(2)').type('Santosh');
+    cy.get('#exampleFormControlSelect1').select('Female');
   });
   it("Test Step 1", () => {
     cy.log('test step 1');
+    
   });
   it("Test Step 2", () => {
     cy.log('test step 1');
